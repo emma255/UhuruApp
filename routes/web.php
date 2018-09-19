f60c5e6729cff86cc1fe6bcbf3027a2e162cbcbf
“[emma255]” <[aemmanuel199@gmail.com>
2018-09-20 02:42:32 +0300

    modifying the index

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/home1', 'Home1Controller@index');
+Route::get('/', 'Home1Controller@index');
