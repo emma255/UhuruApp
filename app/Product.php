6c4b23b7a2beb12c6cb55677b4b56a1a7f02eb70
“[emma255]” <[aemmanuel199@gmail.com>
2018-09-22 04:32:44 +0300

    making database models for the respective tables created

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'price',
+        'location',
+        'contacts',
+        'quantity',
+        'picture',
+        'extra_info',
+        'date_from',
+        'date_to',
+        'category_id',
+    ];
 }
