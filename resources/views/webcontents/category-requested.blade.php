79b76972d598aba19300e4b36f04c891d59cb537
“[emma255]” <[aemmanuel199@gmail.com>
2018-09-22 04:37:06 +0300

    fetching the properties from the database

--- resources/views/webcontents/category-requested.blade.php
+++ resources/views/webcontents/category-requested.blade.php
@@ -1,8 +1,10 @@
-@extends('webcontents.index')
-
+@extends('webcontents.index') 
 @section('contents')
-    <div class="col-md-3 top_brand_left">
+<div class="col-md-3 top_brand_left">
     <div class="hover14 column">
+
+        {{-- description here --}} 
+        @foreach ($products as $product)
         <div class="agile_top_brand_left_grid">
             <div class="agile_top_brand_left_grid_pos">
                 <img src="{{asset('images/offer.png')}}" alt=" " class="img-responsive" />
@@ -12,29 +14,14 @@
                     <div class="snipcart-item block">
                         <div class="snipcart-thumb">
                             <a href="single.html"><img src="{{asset('images/2.png')}}" alt=" " class="img-responsive" /></a>
-                            <p>Pepsi soft drink (2 Ltr)</p>
+                            <p>{{$product->name}} soft drink (2 Ltr)</p>
                             <h4>$8.00 <span>$10.00</span></h4>
-                        </div>
-                        <div class="snipcart-details top_brand_home_details">
-                            <form action="#" method="post">
-                                <fieldset>
-                                    <input type="hidden" name="cmd" value="_cart" />
-                                    <input type="hidden" name="add" value="1" />
-                                    <input type="hidden" name="business" value=" " />
-                                    <input type="hidden" name="item_name" value="Pepsi soft drink" />
-                                    <input type="hidden" name="amount" value="8.00" />
-                                    <input type="hidden" name="discount_amount" value="1.00" />
-                                    <input type="hidden" name="currency_code" value="USD" />
-                                    <input type="hidden" name="return" value=" " />
-                                    <input type="hidden" name="cancel_return" value=" " />
-                                    <input type="submit" name="submit" value="Add to cart" class="button" />
-                                </fieldset>
-                            </form>
                         </div>
                     </div>
                 </figure>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
