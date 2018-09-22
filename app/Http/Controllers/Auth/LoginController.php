35fc6b67aaa272f0924110c0879d0fb52df5fb58
“[emma255]” <[aemmanuel199@gmail.com>
2018-09-22 04:33:46 +0300

    change redirect path to the root

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home1';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
