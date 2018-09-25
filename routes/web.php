1309368a9945e8aeea14504e6a97d7a32e14625d
“[emma255]” <[aemmanuel199@gmail.com>
2018-09-25 05:59:46 +0300

    adding more product and category routes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,30 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('product/view', 'ProductController@index')->name('fetchProduct');
 
-Route::get('product/create', 'ProductController@create')->name('productAdd');
+Route::get('admin/product/view', 'ProductController@indexAdmin')->name('fetchProductAdmin');
 
-Route::post('product/store', 'ProductController@store')->name('productStore');
+Route::get('product/create', 'ProductController@create')->name('productAdd')->middleware('auth');
+
+Route::post('product/store', 'ProductController@store')->name('productStore')->middleware('auth');
+
+Route::get('product/show', 'ProductController@show')->name('productShow');
+
+Route::get('product/edit', 'ProductController@edit')->name('productEdit')->middleware('auth');
+
+Route::post('product/update', 'ProductController@update')->name('productUpdate')->middleware('auth');
+
+Route::get('product/delete', 'ProductController@destroy')->name('productDelete')->middleware('auth');
+
+Route::get('category/create', 'CategoryController@create')->name('addCategory')->middleware('auth');
+
+Route::post('category/store', 'CategoryController@store')->name('storeCategory')->middleware('auth');
+
+Route::get('categories/list', 'CategoryController@index')->name('listCategories')->middleware('auth');
+
+Route::get('category/edit', 'CategoryController@edit')->name('editCategory')->middleware('auth');
+
+Route::post('category/update', 'CategoryController@update')->name('updateCategory')->middleware('auth');
+
+Route::get('category/delete', 'CategoryController@destroy')->name('deleteCategory')->middleware('auth');
+
+Route::get('category/show', 'CategoryController@show')->name('showCategory')->middleware('auth');
