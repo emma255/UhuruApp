c030a38002fdc14d0f0677adc5d163019166a3bd
“[emma255]” <[aemmanuel199@gmail.com>
2018-10-05 09:03:19 +0300

    writting loan functions

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -14,7 +14,9 @@ class LoanController extends Controller
      */
     public function index()
     {
-        //
+        $institutions = Loan::all();
+
+        return view('webcontents.list-loan-institutions', compact('institutions'));
     }
 
     /**
@@ -24,7 +26,7 @@ class LoanController extends Controller
      */
     public function create()
     {
-        //
+        return view('webcontents.loan-institution-create');
     }
 
     /**
@@ -35,7 +37,28 @@ class LoanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'organization_name'=>'required',
+            'collateral'=>'required',
+            'credit_value'=>'required',
+            'minimum_amount'=>'required',
+            'scale'=>'required',
+            'extra'=>'required',
+            'contacts'=>'required',
+        ]);
+        $institution = new Loan;
+
+        $institution->organization_name = $request->organization_name;
+        $institution->collateral = $request->collateral;
+        $institution->credit_value = $request->credit_value;
+        $institution->extra_notes = $request->extra;
+        $institution->minimum_amount = $request->minimum_amount;
+        $institution->scale = $request->scale;
+        $institution->contacts = $request->contacts;
+
+        $institution->save();
+
+        return redirect()->route('listInstitutions');
     }
 
     /**
@@ -44,9 +67,11 @@ class LoanController extends Controller
      * @param  \App\Loan  $loan
      * @return \Illuminate\Http\Response
      */
-    public function show(Loan $loan)
+    public function show()
     {
-        //
+        $institute = Loan::find(request()->id);
+
+        return view('webcontents.admin-loan-institution', compact("institute"));
     }
 
     /**
@@ -55,9 +80,11 @@ class LoanController extends Controller
      * @param  \App\Loan  $loan
      * @return \Illuminate\Http\Response
      */
-    public function edit(Loan $loan)
+    public function edit()
     {
-        //
+        $institution = Loan::find(request()->id);
+
+        return view('webcontents.loan-institution-edit', compact('institution'));
     }
 
     /**
@@ -69,7 +96,30 @@ class LoanController extends Controller
      */
     public function update(Request $request, Loan $loan)
     {
-        //
+        $institution = Loan::find(request()->id);
+
+
+        $this->validate($request,[
+            'organization_name'=>'required',
+            'collateral'=>'required',
+            'credit_value'=>'required',
+            'minimum_amount'=>'required',
+            'scale'=>'required',
+            'extra'=>'required',
+            'contacts'=>'required',
+        ]);
+
+        $institution->organization_name = $request->organization_name;
+        $institution->collateral = $request->collateral;
+        $institution->credit_value = $request->credit_value;
+        $institution->extra_notes = $request->extra;
+        $institution->minimum_amount = $request->minimum_amount;
+        $institution->scale = $request->scale;
+        $institution->contacts = $request->contacts;
+
+        $institution->save();
+
+        return redirect()->route('listInstitutions');
     }
 
     /**
@@ -80,6 +130,51 @@ class LoanController extends Controller
      */
     public function destroy(Loan $loan)
     {
-        //
+        $institution = Loan::find(request()->id);
+
+        $institution->delete();
+
+        return redirect()->route('listInstitutions');
     }
+
+    /**
+     * Return the form for requesting loans.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function loanForm()
+    {
+        return view('webcontents.loan-request-form');
+    }
+    /**
+     * Process loan request.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function requestLoan(Request $request)
+    {
+
+        $institutions = Loan::where([
+            ['collateral',$request->collateral],
+            ['credit_value','<=', $request->credit_value],
+            ['minimum_amount','>=', $request->amount],
+            ['scale',$request->scale]
+        ])->get();
+
+        return view('webcontents.requested-institution', compact('institutions'));
+        // echo $institution;
+    }
+    /**
+     * return the successfully requested institute.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function requestedInstitute()
+    {
+
+        $institute = Loan::find(request()->id);
+
+        return view('webcontents.succeed-institute', compact('institute'));
+    }
+
 }
