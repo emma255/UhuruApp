f896270e4a7fff9c06a0af8f0b46a0c5b8793bd6
“[emma255]” <[aemmanuel199@gmail.com>
2018-10-05 09:06:20 +0300

    adding routes for admin, trainings and loan

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('admin', 'HomeController@admin')->name('admin');
+
+
+// PRODUCTS
+
 Route::get('product/view', 'ProductController@index')->name('fetchProduct');
 
 Route::get('admin/product/view', 'ProductController@indexAdmin')->name('fetchProductAdmin');
@@ -37,6 +42,9 @@ Route::post('product/update', 'ProductController@update')->name('productUpdate')
 
 Route::get('product/delete', 'ProductController@destroy')->name('productDelete')->middleware('auth');
 
+
+// CATEGORIES
+
 Route::get('category/create', 'CategoryController@create')->name('addCategory')->middleware('auth');
 
 Route::post('category/store', 'CategoryController@store')->name('storeCategory')->middleware('auth');
@@ -50,3 +58,41 @@ Route::post('category/update', 'CategoryController@update')->name('updateCategor
 Route::get('category/delete', 'CategoryController@destroy')->name('deleteCategory')->middleware('auth');
 
 Route::get('category/show', 'CategoryController@show')->name('showCategory')->middleware('auth');
+
+// INSTITUTIONS
+
+Route::get('institutions/list', 'LoanController@index')->name('listInstitutions')->middleware('auth');
+
+Route::get('institution/create', 'LoanController@create')->name('addInstitution')->middleware('auth');
+
+Route::post('institution/store', 'LoanController@store')->name('storeInstitution')->middleware('auth');
+
+Route::get('institution/show', 'LoanController@show')->name('showInstitution')->middleware('auth');
+
+Route::get('institution/edit/{id}', 'LoanController@edit')->name('editInstitution')->middleware('auth');
+
+Route::get('institution/delete/{id}', 'LoanController@destroy')->name('deleteInstitution')->middleware('auth');
+
+Route::post('institution/update', 'LoanController@update')->name('updateInstitution')->middleware('auth');
+
+Route::get('loan/request', 'LoanController@loanForm')->name('loanForm')->middleware('auth');
+
+Route::post('loan/request', 'LoanController@requestLoan')->name('loanRequest')->middleware('auth');
+
+Route::get('institute/requested', 'LoanController@requestedInstitute')->name('requestedInstituton')->middleware('auth');
+
+
+// Trainings
+Route::get('trainings/list', 'TrainingController@index')->name('listTrainings')->middleware('auth');
+
+Route::get('training/create', 'TrainingController@create')->name('addTraining')->middleware('auth');
+
+Route::post('training/store', 'TrainingController@store')->name('storeTraining')->middleware('auth');
+
+Route::get('training/show', 'TrainingController@show')->name('showTraining')->middleware('auth');
+
+Route::get('training/edit/{id}', 'TrainingController@edit')->name('editTraining')->middleware('auth');
+
+Route::get('training/delete/{id}', 'TrainingController@destroy')->name('deleteTraining')->middleware('auth');
+
+Route::post('training/update', 'TrainingController@update')->name('updateTraining')->middleware('auth');
