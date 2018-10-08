c69cbea2144f6f5fc54a67862477d1716892a483
“[emma255]” <[aemmanuel199@gmail.com>
2018-10-08 09:13:09 +0300

    editing the listing of products in the view

--- resources/views/webcontents/products-list.blade.php
+++ resources/views/webcontents/products-list.blade.php
@@ -1,31 +1,15 @@
 @extends('layouts.app') 
 @section('contents')
 
-@foreach ($products as $product)
-
-<div class="col-md-3">
-    <div class="hover14 column">
-
-        {{-- description here --}} 
-        <div class="agile_top_brand_left_grid">
-            <div class="agile_top_brand_left_grid_pos">
-                <img src="{{asset('images/offer.png')}}" alt=" " class="img-responsive" />
-            </div>
-            <div class="agile_top_brand_left_grid1">
-                <figure>
-                    <div class="snipcart-item block">
-                            <a href="{{route('productShow',['id'=>$product->id])}}">
-                        <div class="snipcart-thumb">
-                            {{-- <img src="{{ asset('../storage/app/'.$product->picture)}}" alt=" " class="img-responsive"> --}}
-                        <img src="{{ asset('storage/'.$product->picture)}}" alt="{{$product->name}}" class="img-responsive">
-                            <p>{{$product->name}} ({{$product->quantity}})</p>
-                            <p>{{_('@')}}{{$product->price}} <span></span></p>
-                        </div></a>
-                    </div>
-                </figure>
-            </div>
-        </div><br>
+<div class="col-md-12 row">
+    @foreach ($products as $product)
+    <div class="col-md-2">
+        <a href="{{route('productShow',['id'=>$product->id])}}">
+            <img src="{{ asset('storage/'.$product->picture)}}" width="130px" height="130px" alt="{{$product->name}}" class="img-responsive">
+            <p>{{$product->name}} ({{$product->quantity}})</p>
+            <p>{{_('@')}}{{$product->price}} </p>
+        </a>
     </div>
+    @endforeach
 </div>
-@endforeach
 @endsection
