009a4f86184e6981cb08839a0da0fab31abb55a1
“[emma255]” <[aemmanuel199@gmail.com>
2018-10-08 09:05:15 +0300

    adding order details

---
+++ resources/views/orders/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app') 
+@section('contents')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header col-sm-12">Order placing.</div>
+                <div class="card-body">
+                    <p>
+                        Coming soon!!!!!!!!!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
